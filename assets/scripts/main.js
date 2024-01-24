6654e882726f299642e429dab133eb488639d3b6
DEV-LUK4Z <lucassoares1099@outlook.com>
2024-01-24 13:27:05 -0300

    simplificando as funções

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,14 @@
 let texto_area = document.getElementById('texto');
 let resultado = document.getElementById('resultado');
-let novo_texto = [];
-
-
-function mostrar_resultado(){
-    return resultado.innerHTML = novo_texto.join('');
-}
 
 function Criptografar(){
     let texto = texto_area.value;
-
-    novo_texto.push((texto.replace(/i/g, 'imes')).replace(/e/g, 'enter').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat'));
-    mostrar_resultado();
+    resultado.innerHTML = texto.replace(/i/g, 'imes').replace(/e/g, 'enter').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');    
 }
 
 function Descriptografar(){
     let texto = texto_area.value;
-    novo_texto.push((texto.replace(/ai/g, 'a')).replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ufat/g, 'u'));
-    mostrar_resultado();
+    resultado.innerHTML = texto.replace(/ai/g, 'a').replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ufat/g, 'u');
 }
 
 function Copiar_texto(){
