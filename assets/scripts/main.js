246720a1e9213682ced2c66b999cf3e0e9b69447
DEV-LUK4Z <lucassoares1099@outlook.com>
2024-01-24 18:05:55 -0300

    Resolvendo o bug do resultado duplicado

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,24 +4,25 @@ let novo_texto = [];
 
 
 function mostrar_resultado(){
-    resultado.innerHTML = novo_texto.join('');
-    novo_texto = [];
+    return resultado.innerHTML = novo_texto.join('');
 }
 
 function Criptografar(){
     let texto = texto_area.value;
-
+    novo_texto = []
     novo_texto.push((texto.replace(/i/g, 'imes')).replace(/e/g, 'enter').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat'));
     mostrar_resultado();
 }
 
 function Descriptografar(){
     let texto = texto_area.value;
+    novo_texto = []
     novo_texto.push((texto.replace(/ai/g, 'a')).replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ufat/g, 'u'));
     mostrar_resultado();
 }
 
 function Copiar_texto(){
-    navigator.clipboard.writeText(mostrar_resultado())
+    navigator.clipboard.writeText(novo_texto);
     return alert('texto copiado');
 }
+
